12f1ed99da9060bbe90b668c6b27d789dcfd71fd
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-08-30 22:16:02 +0500

    fix

---
+++ app/src/main/java/com/example/spj/presentation/onBoarding/OnBoardingActivity.kt
@@ -0,0 +1,66 @@
+package com.example.spj.presentation.onBoarding
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.spj.R
+import com.example.spj.presentation.MainActivity
+import com.example.spj.presentation.onBoarding.onBoardingAdapter.OnBoardingAdapter
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+        setViewPager()
+        setStatusBarGradiantOnBoardingActivity(this)
+    }
+
+    private fun setViewPager() {
+        val fragmentList = ArrayList<Fragment>()
+        fragmentList.add(ThirstStartFragment())
+        fragmentList.add(SecondFragment())
+        val adapterViewPager = OnBoardingAdapter(
+            fragmentList,
+            this.supportFragmentManager,
+            lifecycle
+        )
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        viewPager.adapter = adapterViewPager
+        val indicator = findViewById<DotsIndicator>(R.id.dots_indicator)
+        indicator.attachTo(viewPager)
+        val finish = findViewById<ImageView>(R.id.finish)
+        finish.setOnClickListener { v: View? ->
+            val intent = Intent(
+                this@OnBoardingActivity,
+                MainActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    fun setStatusBarGradiantOnBoardingActivity(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = activity.window
+            val background = ContextCompat.getDrawable(activity, R.color.background)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+            window.statusBarColor = ContextCompat.getColor(activity,android.R.color.transparent)
+            window.setBackgroundDrawable(background)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spj/presentation/onBoarding/onBoardingAdapter/OnBoardingAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.spj.presentation.onBoarding.onBoardingAdapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class OnBoardingAdapter (arrayList : ArrayList<Fragment>, fm : FragmentManager, lifecycle : Lifecycle)
+    : FragmentStateAdapter(fm, lifecycle) {
+
+    private val fragmentListOnBoarding = arrayList
+
+    override fun getItemCount(): Int = fragmentListOnBoarding.size
+
+    override fun createFragment(position: Int): Fragment = fragmentListOnBoarding[position]
+}
